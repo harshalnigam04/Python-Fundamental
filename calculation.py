051fd6f47c21d05d5b2ce26e9130b522c57beb1e
harshalnigam04 <hnigam33@gmail.com>
2025-11-27 15:46:19 +0530

    Add files via upload

---
+++ calculation.py
@@ -0,0 +1,11 @@
+#1.   Uses a for loop to iterate over numbers from 1 to 50.
+#2.   Calculates the sum of all integers in this range.
+#3.   Displays the final sum.
+
+
+total_sum = 0
+
+for num in range(1, 51):
+    total_sum += num
+
+print("The sum of numbers from 1 to 50 is:", total_sum)
